b476ab6bbd84478a99317778bc358d75659d0519
poojitha atthuri <atthuripoojapavani@gmail.com>
2022-07-12 01:31:33 +0530

    added:modifiedgenerics5

--- generics/Generic1.java
+++ generics/Generic1.java
@@ -4,40 +4,7 @@ import static java.lang.Math.max;
 
 public class Generic1 {
 
-    public Integer testMaxium(Integer x, Integer y, Integer z) {
-        Integer max = x;
-        if (y.compareTo(max) > 0) {
-            max = y;
-        }
-        if (z.compareTo(max) > 0) {
-            max = z;
-        }
-        return max;
-    }
-
-    public Float testMaxium(Float x, Float y, Float z) {
-        Float max = x;
-        if (y.compareTo(max) > 0) {
-            max = y;
-        }
-        if (z.compareTo(max) > 0) {
-            max = z;
-        }
-        return max;
-    }
-
-    public String testMaxium(String x, String y, String z) {
-        String max = x;
-        if (y.compareTo(max) > 0) {
-            max = y;
-        }
-        if (z.compareTo(max) > 0) {
-            max = z;
-        }
-        return max;
-    }
-
-    public class GenericClass<T extends Comparable<T>> {
+    public class GenericsMain<T extends Comparable<T>> {
         public void testMaxium(T... n) {
             Integer i = 0;
             T max = n[i];
@@ -46,8 +13,13 @@ public class Generic1 {
                     max = n[i];
                 i++;
             }
-            System.out.println("The maximum Value is : " + max);
+            PrintMax(max);
+        }
+
+        public void PrintMax(T max) {
+            public <T > void PrintMax (T max){
+                System.out.println("The maximum Value is : " + max);
+            }
         }
     }
 }
-
--- generics/GenericsMain.java
+++ generics/GenericsMain.java
@@ -2,17 +2,16 @@ package generics;
 
 public class GenericsMain {
     public static void main(String[] args) {
-System.out.println("Welcome1 to generics");
-    //UC3 find maximum of three Integers,float and String
-    GenericClass genericClass = new GenericClass();
-        System.out.println("The Maximum Integer is : "+genericClass.testMaxium(1,7,4));
-        System.out.println("The Maximum Float is : "+genericClass.testMaxium(1.3f,4.5f,7.7f));
-        System.out.println("The Maximum String is : "+genericClass.testMaxium("apple","banana","peach"));
-    //UC4 more than three values
-    int in1 =4, in2 =6 , in3 =1, in4 =1;
-    String s1="deer", s2="rino", s3="lion", s4 ="bear";
-    GenericClass genericClass =new GenericClass();
+        System.out.println("Welcome1 to generics");
+        //UC5 Using toPrint method print the more than three values
+        int in1 =4, in2 =7 , in3 =1, in4 =1;
+        String s1="deer", s2="rino", s3="lion", s4 ="bear";
+        float f1=1.3f, f2=6.6f, f3=5.9f, f4=3.3f;
+        GenericClass genericClass =new GenericClass();
+        System.out.println("*********************************");
         genericClass.testMaxium(in1,in2,in3,in4);
-        genericClass.testMaxium(s1,s2,s3);
-}
+        genericClass.testMaxium(s1,s2,s3,s4);
+        genericClass.testMaxium(f1,f2,f3,f4);
+        System.out.println("**********************************");
+    }
 }
