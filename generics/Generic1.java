0a203e81b51d4d062423561866f3b01b9f15c694
poojitha atthuri <atthuripoojapavani@gmail.com>
2022-07-12 01:26:33 +0530

    added:modifiedgenerics4

--- generics/Generic1.java
+++ generics/Generic1.java
@@ -4,39 +4,50 @@ import static java.lang.Math.max;
 
 public class Generic1 {
 
-        private static Integer x;
-        private static Integer y;
-        private static Integer z;
+    public Integer testMaxium(Integer x, Integer y, Integer z) {
+        Integer max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        return max;
+    }
 
-       // public static void main(String args[]) {
-            public Integer testMaxium (Integer x, Integer y, Integer z){
-                Integer max = x;
-                if (y.compareTo(max) > 0) {
-                    max = y;
-                }
-                if (z.compareTo(max) > 0) {
-                    max = z;
-                }
+    public Float testMaxium(Float x, Float y, Float z) {
+        Float max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        return max;
+    }
 
-                return max();
-                class generics {
+    public String testMaxium(String x, String y, String z) {
+        String max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        return max;
+    }
 
-                    public Float testMaxium(Float x, Float y, Float z) {
-                        Float max = x;
-                        if (y.compareTo(max) > 0) {
-                            max = y;
-                        }
-                        if (z.compareTo(max) > 0) {
-                            max = z;
-                        }
-                        return max;
-                    }
-                }}
-
-    private Integer max() {
-        return null;
+    public class GenericClass<T extends Comparable<T>> {
+        public void testMaxium(T... n) {
+            Integer i = 0;
+            T max = n[i];
+            for (T a : n) {
+                if (n[i].compareTo(max) > 0)
+                    max = n[i];
+                i++;
+            }
+            System.out.println("The maximum Value is : " + max);
+        }
     }
 }
-
-
 
--- generics/GenericsMain.java
+++ generics/GenericsMain.java
@@ -1,22 +1,18 @@
 package generics;
 
 public class GenericsMain {
-
     public static void main(String[] args) {
-        System.out.println("Welcome1 to generics");
-        //UC1 find maximum of three Integers
-        //UC2 find maximum of three Integers
-        GenericsMain genericClass = new GenericsMain();
-        System.out.println("The Maximum Integer is : " + genericClass.testMaxium(1, 7, 4));
-        System.out.println("The Maximum Float is : " + genericClass.testMaxium(1.3f, 4.5f, 7.7f));
-
-    }
-
-    private String testMaxium(float v, float v1, float v2) {
-        String o = null;
-        return o;
-    }
-
+System.out.println("Welcome1 to generics");
+    //UC3 find maximum of three Integers,float and String
+    GenericClass genericClass = new GenericClass();
+        System.out.println("The Maximum Integer is : "+genericClass.testMaxium(1,7,4));
+        System.out.println("The Maximum Float is : "+genericClass.testMaxium(1.3f,4.5f,7.7f));
+        System.out.println("The Maximum String is : "+genericClass.testMaxium("apple","banana","peach"));
+    //UC4 more than three values
+    int in1 =4, in2 =6 , in3 =1, in4 =1;
+    String s1="deer", s2="rino", s3="lion", s4 ="bear";
+    GenericClass genericClass =new GenericClass();
+        genericClass.testMaxium(in1,in2,in3,in4);
+        genericClass.testMaxium(s1,s2,s3);
 }
-
-
+}
